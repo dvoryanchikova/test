4dfe5cb29fbb33e73465ba0e638ba24ea6411544
Lerita <dvoryanchikova.lera@gmail.com>
2017-05-30 04:33:50 +0300

    test

---
+++ Assets/TBS Framework/Scripts/Example1/Dragon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MyUnit {
+
+	protected override void Defend(Unit other, int damage)
+	{
+		var realDamage = damage;
+		if (other is Hero)
+			realDamage *= 2;//Hero deals double damage to Dragon.
+
+		base.Defend(other, realDamage);
+	}
+}
